364a874413660c6ebc2f78a89412b189df9a4738
Christy Campbell <christytc10@gmail.com>
2022-03-12 13:13:18 +0000

    Remove text

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Should never really see this, it's just serverless APIs
+      Should never really see this, its just serverless APIs
     </div>
   )
 }
