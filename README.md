7a9661dbd4d8b8caddca3571036c66894a33c67b
Christy Campbell <christytc10@gmail.com>
2022-03-12 13:10:15 +0000

    Parse a bunch of icals

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Race Calendar
 
 I want a nice way to see what racing events are coming up this week. Ultimately I want to make this a slash command in a discord or an automated email at the start of the week but at first I just want to parse some ical files from the internet and give an endpoint to return the data
+
+### Source Data
+
+Starting off I'll use the cal files linked here: https://toomuchracing.com/calendar/
--- pages/api/races.js
+++ pages/api/races.js
@@ -1,3 +1,52 @@
-export default function handler(req, res) {
-  res.status(200).json({ races: 'OK' })
+const axios = require('axios')
+const ical = require('node-ical');
+
+const seriesCalendars = [
+  { 
+    "series": "British GT",
+    "cal": "https://www.google.com/calendar/ical/6bh6kok6g3v97ogr2d1s2g1srs%40group.calendar.google.com/public/basic.ics"
+  },{
+    "series": "Formula 1",
+    "cal": "https://calendar.google.com/calendar/ical/fa9bjl6tu13dd10b066stoo5do%40group.calendar.google.com/public/basic.ics"
+  },{
+    "series": "Formula E",
+    "cal": "https://calendar.google.com/calendar/ical/vno0ntshopq0nmob26db2pcen8%40group.calendar.google.com/public/basic.ics"
+  },{
+    "series": "SRO Intercontinental GT Challenge",
+    "cal": "https://calendar.google.com/calendar/ical/kcelko7ictk6okcf4peougahlo%40group.calendar.google.com/public/basic.ics"
+  }
+]
+
+async function getEvents(calUrl){
+  //parse ical contents from URL
+  const calData = await axios.get(calUrl);
+  const directEvents = ical.sync.parseICS(calData.data);
+  let parsedEvents = []
+  Object.entries(directEvents).forEach(([key, value]) => {
+    console.log(key, value.summary)
+    if(value.start > Date.now()){
+      parsedEvents.push({
+        "series": key,
+        "summary": value.summary,
+        "start": value.start,
+        "end": value.end,
+        "location": value.location,
+      });
+    }
+  })
+  parsedEvents.sort((a, b) => {
+    return a.start - b.start
+  })
+  
+  return parsedEvents
+}
+
+export default async function handler(req, res) {
+  let eventsParsed = {}
+  for (let i = 0; i < seriesCalendars.length; i++) {
+    let events = await getEvents(seriesCalendars[i].cal)
+    eventsParsed[seriesCalendars[i].series] = events
+  }
+  console.log(eventsParsed);
+  res.status(200).json(eventsParsed)
 }
